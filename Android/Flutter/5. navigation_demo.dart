aca9880339cdc7026252ba9412abcc4a169b3fc7
geostorm <77898333+sai-Ga@users.noreply.github.com>
2024-10-19 00:53:35 +0530

    Create 5. navigation_demo.dart

---
+++ Android/Flutter/5. navigation_demo.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(NavigationDemoApp());
+}
+
+class NavigationDemoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Navigation Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: NavigationDemoFirstScreen(),
+    );
+  }
+}
+
+class NavigationDemoFirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('First Screen')),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Go to Second Screen'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => NavigationDemoSecondScreen()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class NavigationDemoSecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Second Screen')),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Go Back'),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
+}
